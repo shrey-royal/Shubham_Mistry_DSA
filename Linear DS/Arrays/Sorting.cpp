2a51745f35248a48d4e2cd1bf29211be3ea1d4c1
Shrey K <srkkadia@gmail.com>
2024-07-16 15:04:08 +0530

    selection, insertion

--- Linear DS/Arrays/Sorting.cpp
+++ Linear DS/Arrays/Sorting.cpp
@@ -46,6 +46,34 @@ void bubbleSort(int arr[], int size) {
     }
 }
 
+void selectionSort(int arr[], int size) {
+    for(int i=0; i<size-1; i++) {
+        int minIndex = i;
+        for(int j=i+1; j<size; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void insertionSort(int arr[], int size) {
+    for(int i=0; i<size; i++) {
+        int key = arr[i];
+        int j = i-1;
+
+        while(j >= 0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int size = sizeof(arr)/sizeof(arr[0]);
@@ -54,7 +82,9 @@ int main() {
     cout << "Unsorted array: ";
     printArray(arr, size);
 
-    bubbleSort(arr, size);
+    // bubbleSort(arr, size);
+    // selectionSort(arr, size);
+    insertionSort(arr, size);
 
     cout << "Sorted array: ";
     printArray(arr, size);

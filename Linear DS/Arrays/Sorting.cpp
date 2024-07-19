d9349d018d13f56d1366ef0d327c0b4c18362b9a
Shrey K <srkkadia@gmail.com>
2024-07-19 15:08:07 +0530

    QuickSort

--- Linear DS/Arrays/Sorting.cpp
+++ Linear DS/Arrays/Sorting.cpp
@@ -74,6 +74,37 @@ void insertionSort(int arr[], int size) {
     }
 }
 
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for(int j=low; j<=high-1; j++) {
+        if(arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i+1], &arr[high]);
+    return (i+1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if(low < high) {
+        int pivot = partition(arr, low, high);
+
+
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot+1, high);
+    }
+}
+
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int size = sizeof(arr)/sizeof(arr[0]);
@@ -84,7 +115,8 @@ int main() {
 
     // bubbleSort(arr, size);
     // selectionSort(arr, size);
-    insertionSort(arr, size);
+    // insertionSort(arr, size);
+    quickSort(arr, 0, size-1);
 
     cout << "Sorted array: ";
     printArray(arr, size);

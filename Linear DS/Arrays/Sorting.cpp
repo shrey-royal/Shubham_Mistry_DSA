45c1aacb019938bd26ef5bbe86b9d23b26d3a297
Shrey K <srkkadia@gmail.com>
2024-07-20 12:32:53 +0530

    Sorting Done (Tasks Given)

--- Linear DS/Arrays/Sorting.cpp
+++ Linear DS/Arrays/Sorting.cpp
@@ -105,6 +105,56 @@ void quickSort(int arr[], int low, int high) {
     }
 }
 
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    for(int i=0; i<n1; i++) {
+        L[i] = arr[left+i];
+    }
+
+    for(int i=0; i<n2; i++) {
+        R[i] = arr[mid + 1 + i];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while(i < n1 && j < n2) {
+        if(L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while(i < n1) {
+        arr[k++] = L[i++];
+    }
+
+    while(j < n2) {
+        arr[k++] = R[j++];
+    }
+
+    delete[] L;
+    delete[] R;
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if(left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int size = sizeof(arr)/sizeof(arr[0]);
@@ -116,7 +166,8 @@ int main() {
     // bubbleSort(arr, size);
     // selectionSort(arr, size);
     // insertionSort(arr, size);
-    quickSort(arr, 0, size-1);
+    // quickSort(arr, 0, size-1);
+    mergeSort(arr, 0, size-1);
 
     cout << "Sorted array: ";
     printArray(arr, size);
